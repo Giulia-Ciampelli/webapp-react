6a28716a1db7b9b2a707fb8a77f3a4202c235cc4
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-12-16 17:42:15 +0100

    docs: add loader style

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Una web app per cercare film e recensioni.
 *Pacchetti*
 - react-router-dom
 - fontawesome-icons
+- ldrs
 
 *Componenti*
 - Header
@@ -24,5 +25,4 @@ Da usare con il backend nella repo webapp-express (CORS già installato)
 
 *Todo*
 - Correggi il formattatore date (il giorno è sbagliato)
-- Metti un loader per connessioni lente
 - Pondera se ha senso che il formattatore di date sia su un context
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,14 @@
+import 'ldrs/hourglass';
+
 export default function Loader() {
     return (
-        <div>
-            Caricamento...
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <l-hourglass
+                size="80"
+                bg-opacity="0.1"
+                speed="1.75"
+                color="#4A6F8C"
+            ></l-hourglass>
         </div>
     )
 }
